23b8f6401fdc9d7473e22ec791a54ec4eee55a9b
Clices <watsonmelokira@gmail.com>
2026-03-16 18:28:40 +0000

    Add CI workflow and complete project

--- Library.MVC/Controllers/HomeController.cs
+++ Library.MVC/Controllers/HomeController.cs
@@ -15,7 +15,6 @@ namespace Library.MVC.Controllers
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
